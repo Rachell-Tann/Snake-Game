e9fbbab907c979a4d222886b24643bcb1354b2fe
Rachell-Tann <91642543+Rachell-Tann@users.noreply.github.com>
2021-10-02 14:35:26 -0400

    Update Board.java

--- SRC/Board.java
+++ SRC/Board.java
@@ -1,12 +1,3 @@
-/*
-*Author: Tan,Rachel
-*Personal Project: Snake Game
-*Brief purpose: The objective is to eat as many apples as possible. 
-*Each time the snake eats an apple its body grows. 
-*There will be a time limit of 5 minutes per game.
-*The snake must avoid the walls and its own body in order to continue the game.
-*/
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
